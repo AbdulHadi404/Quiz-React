18d6240caf830defa5adcf9f0e89f249e20293b2
Abdul Hadi <abdulhadi8369@gmail.com>
2021-09-08 00:15:15 +0500

    Build v1.0

---
+++ build/precache-manifest.b954e33d347ceda62e758f48e76a124f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1317bd0fee47eede2301683c11edc2b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02b4ecc5fa65274d2f1e",
+    "url": "/static/css/main.a75472fe.chunk.css"
+  },
+  {
+    "revision": "727615b27b9d9a721c38",
+    "url": "/static/js/2.b96304c8.chunk.js"
+  },
+  {
+    "revision": "02b4ecc5fa65274d2f1e",
+    "url": "/static/js/main.fa4137a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
